251c2263492a9c8e5bea38eae4c3ca0a79243e4b
shezan <shezan.biz@gmail.com>
2026-07-12 13:52:17 +0530

    Release 2026-07-12 13:52:17

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,68 +1,33 @@
 alphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-encrypted_msg = []
-decrypted_msg = []
 
 def main():
     while True:
         try:
             print("Do you want to (e)ncrypt or (d)ecrypt?")
             choice = input("> ")
+            print("Enter message to encrypt.")
+            message = input("> ")
+            key = int(input("Enter Key: "))
             if choice.upper() == "E":
-                encrypt()
+                caesar(message, key)
             if choice.upper() == "D":
-                decrypt()
+                caesar(message, -key)
         except KeyboardInterrupt:
             print("\nProgram Closing...")
             break
             
 
-def encrypt():
-    encrypted_msg.clear()
-    print("Enter message to encrypt.")
-    message = input("> ")
-    key = int(input("Enter Key: "))
-    for x in message:
-        for i in range(len(alphabets)):
-            if alphabets[i] == x:
-                if (i + key) >= (len(alphabets)):
-                    encrypted_msg.append(alphabets[(i + key) % (len(alphabets))])
-                else:
-                    encrypted_msg.append(alphabets[i + key])
-            elif alphabets[i].upper() == x:
-                if (i + key) >= (len(alphabets)):
-                    encrypted_msg.append(alphabets[(i + key) % (len(alphabets))].upper())
-                else:
-                    encrypted_msg.append(alphabets[i + key].upper())
-
-        if x == " ":
-            encrypted_msg.append(" ")
-    
-    print("".join(encrypted_msg))
-
-
-def decrypt():
-    decrypted_msg.clear()
-    print("Enter message to decrypt.")
-    message = input("> ")
-    key = int(input("Enter Key: "))
-    for x in message:
-        for i in range(len(alphabets)):
-            if alphabets[i] == x:
-                    if (i - key) < -26:
-                        decrypted_msg.append(alphabets[(i - key) % -26])
-                    else:
-                        decrypted_msg.append(alphabets[i - key])
-            elif alphabets[i].upper() == x:
-                if (i - key) < -26:
-                    decrypted_msg.append(alphabets[(i - key) % -26].upper())
-                else:
-                    decrypted_msg.append(alphabets[i - key].upper())            
+def caesar(message, key):
+    result = []
+    for char in message:
+        if char in alphabets: 
+            i = alphabets.index(char)
+            result.append(alphabets[(i + key) % 26])
+        elif char == " ":
+            result.append(" ")
+    print("".join(result))   
 
         
-        if x == " ":
-            decrypted_msg.append(" ")
-
-    print("".join(decrypted_msg))
 
 
 
